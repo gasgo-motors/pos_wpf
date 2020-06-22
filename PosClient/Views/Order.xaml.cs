060d1217684270fc202c2c566a099fc6e450aaa5
Avtandil Rukhadze <avtoruxadze@gmail.com>
2020-06-22 20:50:07 +0400

    ....

--- PosClient/Views/Order.xaml.cs
+++ PosClient/Views/Order.xaml.cs
@@ -28,9 +28,10 @@ using iText.Kernel.Font;
 using iText.IO.Font.Constants;
 using iText.IO.Font;
 using iText.Kernel.Pdf.Canvas.Draw;
-using System.Drawing.Printing;
 using System.Threading;
 using RawPrint;
+using System.Drawing.Printing;
+using Aspose.Pdf.Facades;
 
 namespace PosClient.Views
 {
@@ -360,10 +361,16 @@ namespace PosClient.Views
 
             if (!string.IsNullOrEmpty(App.Current.PosSetting.Settings_Printers))
             {
+                //var pr = "";
+                //foreach (String printer in PrinterSettings.InstalledPrinters)
+                //{
+                //    pr =   printer.ToString();
+                //}
 
                 foreach (var printerName in App.Current.PosSetting.Settings_Printers.Split(',').ToList())
                 {
-                    printToPrinter(printerName, fileName);
+                    //printToPrinter(printerName, fileName);
+                    print1(printerName, fileName);
                     Thread.Sleep(4000);
                 }
                 return;
@@ -376,16 +383,54 @@ namespace PosClient.Views
 
         private void printToPrinter(string printerName, string filePath)
         {
-
-            //IPrinter printer = new Printer();
-            //printer.PrintRawFile(printerName, filePath, "print");
             Spire.Pdf.PdfDocument pdfdocument = new Spire.Pdf.PdfDocument();
             pdfdocument.LoadFromFile(filePath);
             pdfdocument.PrinterName = printerName;
+
+            PaperSize paperSize = new PaperSize();
+            paperSize.Width = 283;//inch*100
+            paperSize.Height = 826;//inch*100
+            paperSize.RawKind = (int)PaperKind.Custom;
+          //  pdfdocument.PrinterSettings.PaperSize = paperSize;
+
             //pdfdocument.war
             pdfdocument.PrintDocument.PrinterSettings.Copies = 1;
             pdfdocument.PrintDocument.Print();
             pdfdocument.Dispose();
+        }
+
+        public void print1(string printerName, string filePath)
+        {
+           // string dataDir = RunExamples.GetDataDir_AsposePdfFacades_Printing();
+            // Create PdfViewer object
+            PdfViewer viewer = new PdfViewer();
+            // Open input PDF file
+            viewer.BindPdf(filePath);
+            // Set attributes for printing
+            viewer.AutoResize = true;         // Print the file with adjusted size
+            viewer.AutoRotate = true;         // Print the file with adjusted rotation
+            viewer.PrintPageDialog = false;   // Do not produce the page number dialog when printing
+            // Create objects for printer and page settings and PrintDocument
+            System.Drawing.Printing.PrinterSettings ps = new System.Drawing.Printing.PrinterSettings();
+            System.Drawing.Printing.PageSettings pgs = new System.Drawing.Printing.PageSettings();
+            System.Drawing.Printing.PrintDocument prtdoc = new System.Drawing.Printing.PrintDocument();
+            // Set printer name
+            ps.PrinterName = prtdoc.PrinterSettings.PrinterName;
+            // Set PageSize (if required)
+            pgs.PaperSize = new System.Drawing.Printing.PaperSize("Custom", 350, 1000);
+            // Set PageMargins (if required)
+            pgs.Margins = new System.Drawing.Printing.Margins(0, 0, 100, 200);
+            // ExStart:PrintDialog
+            //System.Windows.Forms.PrintDialog printDialog = new System.Windows.Forms.PrintDialog();
+            //if (printDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+                // Document printing code goes here
+                // Print document using printer and page settings
+                viewer.PrintDocumentWithSettings(pgs, ps);
+            //}
+            // ExEnd:PrintDialog
+            // Close the PDF file after priting
+            viewer.Close();
         }
 
 
